e9d9668e371f9437228991be20c6cb488392009e
Prem Mourya <premmourya998@gmail.com>
2025-01-26 02:21:22 +0530

    style and css changed

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -15,20 +15,20 @@ const LandingPage = () => {
         }}
       >
         {/* Main content goes here */}
-        <h2 style={{ fontWeight: "bolder", fontSize: "46px" }}>
+        <h2 style={{ fontWeight: "bolder", fontSize: "40px" }}>
           Welcome to Madrocket Assignment Submission by Prem Mourya 🤗 .
         </h2>
-        <h4 style={{ fontWeight: "bold", fontSize: "40px", color: "#2c3e50" }}>
+        <h4 style={{ fontWeight: "bold", fontSize: "30px", color: "#2c3e50" }}>
           The dashboard have sidebar with 2 options :
         </h4>
-        <h6 style={{ fontSize: "25px", color: "#2c3e50" }}>
+        <h6 style={{ fontSize: "20px", color: "#2c3e50" }}>
           i. Students Page : When students page is clicked, students page will
           open,
         </h6>
-        <h6 style={{ fontSize: "25px", color: "#2c3e50" }}>
+        <h6 style={{ fontSize: "20px", color: "#2c3e50" }}>
           ii.Logout : When Logout button is pressed user will get logged out.
         </h6>
-        <h6 style={{ fontSize: "25px", color: "red" }}>
+        <h6 style={{ fontSize: "20px", color: "red" }}>
           *Note: Please wait after any Activity. Data will load... and then render in
           UI 🤗.
         </h6>
